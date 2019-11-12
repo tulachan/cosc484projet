056a6e24e2a8064c586e119d4a54051124ed90f7
bchali1 <47155146+bchali1@users.noreply.github.com>
2019-11-11 19:10:46 -0500

    Update UsersPage.js

--- Readit React Front End/src/UsersPage.js
+++ Readit React Front End/src/UsersPage.js
@@ -1,1 +1,48 @@
+import React, { Component } from 'react';
+import Login from
+'./login';
+
+import { Link } from 'react-router-dom';
+
+class UsersPage extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            status: "",
+            username:""
+
+        };
+    }
+    handleChangeUser = (event) => {
+        this.setState({status: event.target.value});
+      }
+      handleChangeUser = (event) => {
+          this.setState({username: event.Login.value});
+    }
+    //handleChangePass = (event) => {
+      //  this.setState({password: event.target.value});
+    //}
+
+    render() {
+        return(
+            <div>
+                <h1> Users Page</h1>
+                <form>
+                    <h2> Hello {this.props.username}!</h2>
+                    <label> UpdateStatus </label>
+                    <input name type='text' onChange={this.handleChangeUser} />
+                    <br></br>
+                    <br></br>
+                    <Link to="/home">
+                        <button> Post </button>
+                    </Link>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UsersPage;
 
