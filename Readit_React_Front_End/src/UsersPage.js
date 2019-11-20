0e19a36da3800f7cf537e6ffdd2c23778696e954
bchali1 <47155146+bchali1@users.noreply.github.com>
2019-11-20 14:23:52 -0500

    User can post status and add user name

--- Readit_React_Front_End/src/UsersPage.js
+++ Readit_React_Front_End/src/UsersPage.js
@@ -1,48 +1,124 @@
 import React, { Component } from 'react';
 import './userpage.css';
-import Login from
-'./login';
+
 
 import { Link } from 'react-router-dom';
 
 class UsersPage extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            status: "",
-            username:""
+constructor() {
+    super();
+    
+    this.state = {
+      showComments: false,
+      comments: [
+        {id: 1, author: "COSC484", body: "This is working"}
+        
+      ]
+    };
+  }
+  
+  render () {
+    const comments = this._getComments();
+    let commentNodes;
+    let buttonText = 'Show Status';
+    
+    if (this.state.showComments) {
+      buttonText = 'Hide Status';
+      commentNodes = <div className="comment-list">{comments}</div>;
+    }
+    
+    return(
+      <div className="comment-box">
+        <h2>Post the Status!</h2>
+        <CommentForm addComment={this._addComment.bind(this)}/>
+        <button id="comment-reveal" onClick={this._handleClick.bind(this)}>
+          {buttonText}
+        </button>
+        <h3>Status</h3>
+        <h4 className="comment-count">
+          {this._getCommentsTitle(comments.length)}
+        </h4>
+        {commentNodes}
+      </div>  
+    );
+  } // end render
+  
+  _addComment(author, body) {
+    const comment = {
+      id: this.state.comments.length + 1,
+      author,
+      body
+    };
+    this.setState({ comments: this.state.comments.concat([comment]) }); 
+  }
+  
+  _handleClick() {
+    this.setState({
+      showComments: !this.state.showComments
+    });
+  }
+  
+  _getComments() {    
+    return this.state.comments.map((comment) => { 
+      return (
+        <Comment 
+          author={comment.author} 
+          body={comment.body} 
+          key={comment.id} />
+      ); 
+    });
+  }
+  
+  _getCommentsTitle(commentCount) {
+    if (commentCount === 0) {
+      return 'No Status yet';
+    } else if (commentCount === 1) {
+      return "1 Status";
+    } else {
+      return `${commentCount} Status`;
+    }
+  }
+} // end CommentBox component
 
-        };
-    }
-    handleChangeUser = (event) => {
-        this.setState({status: event.target.value});
-      }
-      handleChangeUser = (event) => {
-          this.setState({username: event.target.value});
-    }
-    //handleChangePass = (event) => {
-      //  this.setState({password: event.target.value});
-    //}
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
+        <div className="comment-form-fields">
+          <input placeholder="Name" required ref={(input) => this._author = input}></input><br />
+          <textarea placeholder="Comment" rows="4" required ref={(textarea) => this._body = textarea}></textarea>
+        </div>
+        <div className="comment-form-actions">
+          <button type="submit">Post Status</button>
+        </div>
+      </form>
+    );
+  } // end render
+  
+  _handleSubmit(event) { 
+    event.preventDefault();   // prevents page from reloading on submit
+    let author = this._author;
+    let body = this._body;
+    this.props.addComment(author.value, body.value);
+  }
+} // end CommentForm component
 
-    render() {
-        return(
-            <div className="user-textbox">
-                <h1 className="user-title"> Users Page</h1>
-                <form className="user-form">
-                    <h2 className="user-hello"> Hello {this.props.username}!</h2>
-                    <label className="user-status"> UpdateStatus </label>
-                    <input name type='text' onChange={this.handleChangeUser} />
-                    <br></br>
-                    <br></br>
-                    <Link to="/home">
-                        <button className="post"> Post </button>
-                    </Link>
-                </form>
-            </div>
-        )
-    }
+class Comment extends React.Component {
+  render () {
+    return(
+      <div className="comment">
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">- {this.props.body}</p>
+        <div className="comment-footer">
+          <a href="#" className="comment-footer-delete" onClick={this._deleteComment}>Delete Comment</a>
+        </div>
+      </div>
+    );
+  }
+  _deleteComment() {
+    alert("Comment Deleted");
+  }
 }
 
 export default UsersPage;
