015c8ae1a41b3ee983006a0602b8864c54a2ad26
Jacob Della <jdella@Jacobs-MBP.fios-router.home>
2019-11-24 17:48:04 -0500

    Added upvote / downvote, misc endpoints for sorting posts, new subreadit and new post now use get instead of post

--- Readit_Middleware_API/api.js
+++ Readit_Middleware_API/api.js
@@ -44,37 +44,39 @@ app.get('/start', (req, res) => {
 
                                              
 // Create sub-readit using a post request.
-app.post('/api/newpost', function(req, res) {
+app.get('/api/newpost', function(req, res) {
 	postauthor = req.session.username;
-	postbody = req.session.postbody;
-	posttitle = req.session.posttitle;
-	postsubreadit = req.session.postsubreadit;
+	postbody = req.param('postbody');
+	posttitle = req.param('posttitle');
+	postsubreadit = req.param('postsubreadit');
 	//If not logged in can't create subreddit.
 	if (req.session.loggedin) {
 		database.cfg.query('SELECT * FROM subreadits WHERE subreadit_name = ?', [postsubreadit], function(err, results, fields){
 			if(results[0] == null){
+				res.send({ message: 'Failed to post'});
+			} else {		
+				console.log(results);
 			database.cfg.query('INSERT INTO posts (post_body, post_title, post_subreadit, post_author) VALUES (? ,?,?,?)'
-				,[postbody, posttitle, postsubreadit, postauthor], function(err, results, fields) {
-					if (!err){
-						res.send({ message: 'Post successfuly created!'});
-					} else {
-						res.send({ message: 'Failed to post'});
-					}
-				});
-			}	
-			
-			else {
+			,[postbody, posttitle, postsubreadit, postauthor], function(err, results, fields) {
+				if (!err){
+					res.send({ message: 'Post successfuly created!'});
+				} else {
+					console.log(err);
 					res.send({ message: 'Failed to post'});
 				}
+			});
+			}
 		});
+	} else {
+			res.send({ message: 'You need to be logged in to do that.'});
 	}
 	});
 
 //Displays top posts on whole site with a start at 1000 likes.
 app.get('/api/topposts', function(req, res) {
-	let likerange = 1000;
+	let likerange = req.param('likerange');
 	database.cfg.query('SELECT * FROM posts WHERE post_likes >= ?', [likerange], function(err, results, fields){
-		if(err){
+		if(results.length == 0){
 			res.send({ message: 'Failed to get top posts'});
 		} else {
 			res.send(results);
@@ -82,14 +84,41 @@ app.get('/api/topposts', function(req, res) {
 	});
 });
 
+//Displays post of a specific ID.
+app.get('/api/displaypostbyid', function(req, res) {
+	let postid = req.param('postid');
+	database.cfg.query('SELECT * FROM posts WHERE post_id = ?', [postid], function(err, results, fields){
+		if(results.length == 0){
+			res.send({message: 'Failed to find post'});
+		} else {
+			res.send(results);
+		}
+	});
+});
+
+//Displays post of a specific user.
+app.get('/api/displaypostbyauthor', function(req, res) {
+	let postauthor = req.param('postauthor');
+	database.cfg.query('SELECT * FROM posts WHERE post_author = ?', [postauthor], function(err, results, fields){
+		if(results.length == 0){
+			res.send({message: 'Failed to find posts by this user'});
+		} else {
+			console.log(results.length);
+			res.send(results);
+		}
+	});
+});
+
+
 
 //Displays the subreadit
 app.get('/api/displaysubreadit', function(req, res) {    
 	let subreadit = req.param('subreadit');
 	database.cfg.query('SELECT * FROM posts WHERE post_subreadit = ?', [subreadit], function(err, results, fields){
-		if(results[0] == null){
+		if(err){
 			res.send({message: 'Failed to get subreadit posts'});
 		} else {
+			console.log(results[0]);
 			res.send(results);
 		}
 	});
@@ -97,17 +126,17 @@ app.get('/api/displaysubreadit', function(req, res) {
 
 
 //Creates new subreadit and makes the creator a moderator on said subreadit.
-app.post('/api/newsubreadit', function(req, res) {
+app.get('/api/newsubreadit', function(req, res) {
 	subreaditmod = req.session.username;
-	subreaditname = req.session.subreaditname;
+	subreaditname = req.param('subreaditname');
 	//If not logged in can't create subreddit.
 	if (req.session.loggedin) {
 			database.cfg.query('INSERT INTO subreadits (subreadit_name, subreadit_moderatorname) VALUES (? ,?)'
 				,[subreaditname, subreaditmod], function(err, results, fields) {
 					if (!err){
-						res.send({ message: 'Post successfuly created!'});
+						res.send({ message: 'Subreadit successfuly created!'});
 					} else {
-						res.send({ message: 'Failed to post'});
+						res.send({ message: 'Failed to create subreadit'});
 					}
 				});
 			}
@@ -160,28 +189,29 @@ app.get('/api/apitest', function(req, res) {
 });
 
 // Auth function
-app.post('/api/auth', function(req, res) {
-	let username = req.body.username;
-	let password = req.body.password;
-	//Login stuff
-	if (username && password) {
-		//How you send MySQL queries to the database.
-		database.cfg.query('SELECT * FROM accounts WHERE account_username = ? AND account_password = ?', [username, password], function(err, results, fields){
-			if (results.length > 0) {
-				req.session.loggedin = true;
-				req.session.username = username;
-				res.send({ message: 'Please enter a user name and password' });
-
-				//Redirects us to the home path
-			} else {
-				res.send({ message: 'Please enter a user name and password' });
-			}			
-			res.send();
-		});
+app.get('/api/auth', function(req, res) {
+	let username = req.session.username;
+	let password = req.session.password;
+	req.session.username = username;
+	if(!req.session.loggedin){
+		if (username && password) {
+			//How you send MySQL queries to the database.
+			database.cfg.query('SELECT * FROM accounts WHERE account_username = ? AND account_password = ?', [username, password], function(err, results, fields){
+				if (results.length != 0) {
+					req.session.loggedin = true;
+					res.send({ message: 'Login succesful, welcome ' + req.session.username});
+					//Redirects us to the home path
+				} else {
+					res.send({ message: 'Invalid password or username' });
+				}			
+			});
+		} else {
+			//No password
+			res.send({ message: 'Please enter a user name and password' });
+		}
 	} else {
 		//No password
-		res.send({ message: 'Please enter a user name and password' });
-		response.end();
+		res.send({ message: 'You are already logged in, please logout' });
 	}
 });
 
@@ -203,29 +233,28 @@ app.post('/api/registernewuser', function(req, res) {
 				console.log("Username: " + username);
 				
 			} else {
-				res.send({ message: 'Please enter valid user name and password' });
+				res.send({ message: 'Something went wrong, account probably exists' });
 			}
 		});
 	} else {
 		//No password
 		res.send({ message: 'Please enter Username and Password.'});
-		res.end();
 	}
 });
 
 //Upvotes a post
-app.post('/api/upvotepost', function(req, res) {
-	let postid = req.body.postid;
-	let postlikes = req.body.postlikes;
+app.get('/api/upvotepost', function(req, res) {
+	let postid = req.param('postid');
 	let postmessage = "";
 	if (req.session.loggedin) {
 		// Insert the provided values into respective fields.
 		database.cfg.query('SELECT * FROM posts WHERE post_id = ?', [postid], function(err, results, fields){
-			if (results.length > 0) {
+			if (results[0] != null) {
+			let postlikes = results[0].post_likes + 1;
 			//if we have results and no errors we continue
 			if (!err){
 				//Update postlikes
-				postlikes = postlikes + 1;
+
 				database.cfg.query('UPDATE posts SET post_likes = ? WHERE post_id = ?', [postlikes, postid], function (err, results, fields) {
 					if (err) {
 						postmessage = "Upvoting post with ID " + postid + "failed.";
@@ -236,8 +265,13 @@ app.post('/api/upvotepost', function(req, res) {
 				res.send({ message: postmessage});
 			} else {
 				postmessage = "Upvoting post with ID " + postid + "failed.";
-				res.send({ message: postmessage});			}
+				res.send({ message: postmessage});			
 			}
+		
+
+			
+		}
+
 		});
 	} else {
 		res.send({ message: 'You need to be logged into to do that'});
@@ -245,37 +279,37 @@ app.post('/api/upvotepost', function(req, res) {
 });
 
 
-
-
-app.post('/api/downvotepost', function(req, res) {
-	let postid = req.body.postid;
-	let postlikes = req.body.postlikes;
-	let postmessage = "";
-	if (req.session.loggedin) {
-		// Insert the provided values into respective fields.
-		database.cfg.query('SELECT * FROM posts WHERE post_id = ?', [postid], function(err, results, fields){
-			if (results.length > 0) {
-			//if we have results and no errors we continue
-			if (!err){
-				//Update postlikes
-				postlikes = postlikes - 1;
-				database.cfg.query('UPDATE posts SET post_likes = ? WHERE post_id = ?', [postlikes, postid], function (err, results, fields) {
-					if (err) {
-						postmessage = "Downvoting post with ID " + postid + "failed.";
-						res.send({ message: postmessage});
-					}
-				  });
-				postmessage = "Downvoting post with ID " + postid + "succeeded.";
-				res.send({ message: postmessage});
-			} else {
-				postmessage = "Downvoting post with ID " + postid + "failed.";
-				res.send({ message: postmessage});			}
-			}
-		});
-	} else {
-		res.send({ message: 'You need to be logged into to do that'});
+app.get('/api/downvotepost', function(req, res) {
+		let postid = req.param('postid');
+		let postmessage = "";
+		if (req.session.loggedin) {
+			// Insert the provided values into respective fields.
+			database.cfg.query('SELECT * FROM posts WHERE post_id = ?', [postid], function(err, results, fields){
+				if (results[0] != null) {
+				let postlikes = results[0].post_likes - 1;
+				//if we have results and no errors we continue
+				if (!err){
+					//Update postlikes
+					database.cfg.query('UPDATE posts SET post_likes = ? WHERE post_id = ?', [postlikes, postid], function (err, results, fields) {
+						if (err) {
+							postmessage = "Down post with ID " + postid + " failed.";
+							res.send({ message: postmessage});
+						}
+					  });
+					postmessage = "Down post with ID " + postid + " succeeded.";
+					res.send({ message: postmessage});
+				} else {
+					postmessage = "Down post with ID " + postid + " failed.";
+					res.send({ message: postmessage});			
+				}
 	}
-});
+	
+	});
+		} else {
+			res.send({ message: 'You need to be logged into to do that'});
+		}
+	});
+	
 
 
 
