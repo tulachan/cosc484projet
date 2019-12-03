8dc13fdb4551a5ec2f054726857090ad6ade35b8
t-ambur <trevoramburgey@gmail.com>
2019-12-02 22:12:11 -0500

    reseting to e9d7df8

--- Readit_React_Front_End/src/frontpage.js
+++ Readit_React_Front_End/src/frontpage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { is } from '@babel/types';
 import './frontpage.css';
 // This page dynamically loads the top posts from the database
 
@@ -54,12 +53,11 @@ class FrontPage extends React.Component
               likes={post.post_likes}
               title={post.post_title}
               date={post.post_creationdate}
-              id={post.post_id}
+              subreadit={post.post_subreadit}
               islink={post.post_islink} />
              
              </h4>
               </div>
-
           ); 
         });
       }
@@ -80,106 +78,17 @@ class FrontPage extends React.Component
 }
 
 class TopPosts extends React.Component {
-  constructor(props)
-  {
-      super(props);
-      this.state = {
-          count: 0, 
-          updated: false,
-          author: "",
-          likess: "",
-          id: "",
-          loggedin: false,
-          
-      };
-  }
-
-  componentDidMount() {
-    // Call our fetch function below once the component mounts
-  this.callBackendAPI()
-    .then(res => this.setState({ loggedin: res.loggedin }))
-    .catch(err => console.log(err));
-}
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js)
-callBackendAPI = async () => {
-  const response = await fetch('/api/isloggedin');
-  const body = await response.json();
-
-  if (response.status !== 200) {
-    throw Error(body.message) 
-  }
-  return body;
-};
-handleChangeUser = (event) => {
-  this.setState({id: event.props.array.id});
-}
-handleChangeAuthor = (event) => {
-  this.setState({id: event.props.array.author});
-}
-handlelikeIncrement= (event) =>
-{
-  if(!this.state.updated && this.state.loggedin)
-  {
-    let newCount = this.state.count + 1
-    this.setState({count: newCount});
-    //this.updateDatabase();
-    this.setState({updated: true});
-  }
-  else
-  {
-        alert("Must log in to like or you can't like it again");
-          
-  }
- 
-}
-
   render () {
     return(
-      <div>
-          <p> {this.getType(this.props.islink)}  Author: {this.props.author} 
-         _Sub: {this.props.subreadit} Likes: {this.props.likes} Created: {this.props.date}
-  </p>
       <div className='frontpage2'>
           <p> {this.getType(this.props.islink)} <br></br> <br></br> 
           Author: {this.props.author} <br></br>
          _Subreadit: {this.props.subreadit} <br></br>
            Likes: {this.props.likes}<br></br>
           Created: {this.props.date}
-
          </p> 
-         Likes: {this.state.count}<br></br>
-         <button onClick={this.handlelikeIncrement}>Like</button>
-        </div>
         </div>
     );
-  }
-  //sending data
-  updateDatabase()
-    {
-    
-      if(this.state.loggedin)
-      {
-        
-        fetch('/api/upvotepost', {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(this.state)
-            })
-            .then((result) => result.json())
-            .then((info) => { console.log(info); })
-    }
-    else{
-      return(
-        <div>
-            <h1> You must be logged in to Like! </h1>
-            
-
-        </div>
-      )
-
-    }
   }
 
   display(body)
